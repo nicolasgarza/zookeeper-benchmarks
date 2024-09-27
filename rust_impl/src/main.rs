b0ac5aa1a7359bbc88710f0e808ba79c3deca4f9
Nicolas Garza <nicolas.garza@gmail.com>
2024-09-27 10:25:58 -0500

    working benchmark w 50 threads

--- rust_impl/src/main.rs
+++ rust_impl/src/main.rs
@@ -8,14 +8,19 @@ impl Watcher for DefaultWatcher {
 }
 
 fn main() {
-    let duration = Duration::new(5, 0);
+    ephemeral_test();
+}
+
+fn ephemeral_test() {
+    let duration = Duration::new(10, 0);
     let start_time = Instant::now();
 
-    let zk = ZooKeeper::connect("127.0.0.1:2181", Duration::from_secs(5), DefaultWatcher).unwrap();
+    let zk = ZooKeeper::connect("127.0.0.1:2181", Duration::from_secs(10), DefaultWatcher).unwrap();
 
     let mut handles = Vec::new();
     const BENCHMARK_PATH: &str = "/benchmark";
 
+    // delete old benchmark node if it exists
     if let Ok(prev_benchmark_node) = zk.exists(BENCHMARK_PATH, false) {
         zk.delete(BENCHMARK_PATH, Some(prev_benchmark_node.unwrap().version))
             .expect("Failed to delete old benchmark node");
@@ -29,13 +34,17 @@ fn main() {
     )
     .expect("failed to create parent dir");
 
-    for _ in 0..10 {
+    // store connections in a vec (or else all ephemeral nodes get deleted when threads finish up)
+    let mut zookeeper_connections = Vec::new();
+    for _ in 0..50 {
         let start_time_clone = start_time;
 
+        // spawn the 50 threads
         let handle = thread::spawn(move || {
-            let zk = ZooKeeper::connect("127.0.0.1:2181", Duration::from_secs(5), DefaultWatcher)
+            let zk = ZooKeeper::connect("127.0.0.1:2181", Duration::from_secs(10), DefaultWatcher)
                 .unwrap();
 
+            // create znodes in a loop for each thread
             while start_time_clone.elapsed() < duration {
                 // create an ephemeral sequential znode
                 zk.create(
@@ -46,15 +55,18 @@ fn main() {
                 )
                 .expect("Error creating znode");
             }
+            zk
         });
         handles.push(handle);
     }
 
     // wait for all threads to complete
     for handle in handles {
-        handle.join().unwrap();
+        let zk_connection = handle.join().unwrap();
+        zookeeper_connections.push(zk_connection);
     }
 
+    // get number of children and print them
     let num_children = zk.get_children(BENCHMARK_PATH, false).unwrap().len();
     println!("Total znodes added: {}", num_children);
     println!(
